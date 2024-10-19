5865bb09d256267f9f9751d178f374c46c7e91ee
Elijah Mendez <elijahmendez2022@gmail.com>
2024-10-19 13:53:36 -0400

    Added accordion component.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar"
 import Button from "./components/Button"
+import Accordion from "./components/Accordion"
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <div>
       <Navbar />
       <Button />
+      <Accordion />
       </div>
     </div>
   )
---
+++ front-end/src/components/Accordion.jsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionActions from "@mui/material/AccordionActions";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Button from "@mui/material/Button";
+
+export default function AccordionUsage() {
+  return (
+    <div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          Step 1
+        </AccordionSummary>
+        <AccordionDetails>Walk to the bus stop.</AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2-content"
+          id="panel2-header"
+        >
+          Step 2
+        </AccordionSummary>
+        <AccordionDetails>Get on the bus.</AccordionDetails>
+      </Accordion>
+      <Accordion defaultExpanded>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+        >
+          Step 3
+        </AccordionSummary>
+        <AccordionDetails>Get off of the bus.</AccordionDetails>
+        <AccordionActions>
+          <Button>Cancel</Button>
+          <Button>Agree</Button>
+        </AccordionActions>
+      </Accordion>
+    </div>
+  );
+}
