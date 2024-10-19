c6da684d706b9620e3fc84d4cf36b8bffb4a822a
Elijah Mendez <elijahmendez2022@gmail.com>
2024-10-19 13:06:41 -0400

    Nav bar added.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Home from "./pages/Home";
+import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
     <div className="App">
       <div>
-      <Home />
+      <Navbar />
       </div>
     </div>
   )
