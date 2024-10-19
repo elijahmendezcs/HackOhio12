fbacf596402112735beec59c6e20d6bf8238fc2a
Mohamed Chakrowf <mohamed.chakrowf@outlook.com>
2024-10-19 16:13:05 -0400

    name

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,20 +1,17 @@
 import React from "react";
-import Navbar from "./components/Navbar"
-import Button from "./components/Button"
-
+import Navbar from "./components/Navbar";
+import Button from "./components/Button";
 
 function App() {
-
   return (
     <div className="App">
       <div>
-      <Navbar />
-      
-
-      {/* <Button /> */}
+        <Navbar />
+        String name = "Mo";
+        {/* <Button /> */}
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
