ef803729e49d7436e24072cf7231551334fc76ce
Elijah Mendez <elijahmendez2022@gmail.com>
2024-10-19 11:28:39 -0400

    Added components and pages, ready for work.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
+import Home from "./pages/Home";
 
 function App() {
 
   return (
     <div className="App">
       <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Home />
       </div>
     </div>
   )
